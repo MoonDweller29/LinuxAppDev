bf40dc19fdb175d820312e9a1734d167bd92c79a
Andrew Astapov <andrey.astapov@graphics.cs.msu.ru>
2020-10-02 19:32:08 +0300

    03_TerminalProject: Show works
    "Show" can now show file.
    It manages left/right/up/down arrows, page up/down keys.
    It also converts tabs into spaces.
    
    TODO: wchar_t support

--- 03_TerminalProject/Show.c
+++ 03_TerminalProject/Show.c
@@ -1,28 +1,47 @@
 #include <ncurses.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 #define BUF_SIZE 10
 #define DX 3
+int TAB_SIZE = 4;
 
-// enum key_code
-// {
+#define MIN(X, Y) X < Y ? X : Y
+#define MAX(X, Y) X < Y ? Y : X
+
+
+enum key_code
+{
 // 	KEY_RIGHT   =  261,
 // 	KEY_LEFT    =  260,
 // 	KEY_UP      =  259,
 // 	KEY_DOWN    =  258,
 // 	KEY_NPAGE   =  338,
 // 	KEY_PPAGE   =  339,
-// 	KEY_ESCAPE  =  27
-// }
+	KEY_SPACE   =  32,
+	KEY_ESC     =  27
+};
+
 struct Text
 {
 	int line_count;
+	int max_line_len;
 	char **lines;
 };
 typedef struct Text Text;
 
-char *read_line(FILE *f, int *finished)
+struct Page
+{
+	int first_line;
+	int last_line;
+	int height;
+	int horizontal_offs;
+};
+typedef struct Page Page;
+
+char *read_line(FILE *f, int *finished, int *line_len)
 {
 	*finished = 0;
 	int buf_size = BUF_SIZE;
@@ -32,13 +51,22 @@ char *read_line(FILE *f, int *finished)
 	int i = 0;
 	while (((c=fgetc(f)) != '\n') && (c != EOF))
 	{
-		if (i >= buf_size)
+		int repeat = 1;
+		if (c == '\t')
 		{
-			buf_size += BUF_SIZE;
-			line = (char *) realloc (line, buf_size);
+			repeat = TAB_SIZE;
+			c = ' ';
 		}
-		line[i] = c;
-		++i;
+		for (int j = 0; j < repeat; ++j)
+		{
+			if (i >= buf_size)
+			{
+				buf_size += BUF_SIZE;
+				line = (char *) realloc (line, buf_size);
+			}
+			line[i] = c;
+			++i;
+		}
 	}
 
 	if(c == EOF){ *finished = 1; }
@@ -49,6 +77,7 @@ char *read_line(FILE *f, int *finished)
 		line = (char *) realloc (line, buf_size);
 	}
 	line[i] = '\0';
+	*line_len = i;
 
 	return line;
 }
@@ -56,6 +85,7 @@ char *read_line(FILE *f, int *finished)
 Text read_file(const char *filename)
 {
 	Text text;
+	text.max_line_len = 0;
 	FILE *f = fopen(filename, "r");
 	if (f == NULL)
 	{
@@ -73,9 +103,11 @@ Text read_file(const char *filename)
 		if (i >= buf_size)
 		{
 			buf_size += BUF_SIZE;
-			char **lines = (char **) realloc(lines, buf_size*sizeof(char*));
+			lines = (char **) realloc(lines, buf_size*sizeof(char*));
 		}
-		lines[i] = read_line(f, &eof_reached);
+		int curr_line_len;
+		lines[i] = read_line(f, &eof_reached, &curr_line_len);
+		text.max_line_len = MAX(curr_line_len, text.max_line_len);
 		++i;
 	}
 	text.line_count = i;
@@ -103,38 +135,131 @@ void w_add_nchars(WINDOW *win, const char *str, uint32_t n)
 	}
 }
 
+Page create_page(int first_line, int line_count, int text_line_count, int horizontal_offs)
+{
+	Page page;
+	page.height = line_count;
+	page.first_line = MIN(first_line, text_line_count-1);
+	page.last_line = MIN(first_line + line_count -1, text_line_count-1);
+	page.horizontal_offs = horizontal_offs;
+
+	return page;
+}
+
+WINDOW *dbg_win;
+Page page_up(Page page, int line_shift)
+{
+	int actual_shift = MIN(page.first_line, line_shift);
+	page.first_line -= actual_shift;
+	page.last_line -= actual_shift;
+
+	return page;
+}
+
+Page page_down(Page page, int line_shift, int text_line_count)
+{
+	int actual_shift = MIN(line_shift, text_line_count-1 - page.last_line);
+	page.first_line += actual_shift;
+	page.last_line += actual_shift;
+
+	return page;
+}
+
+Page page_left(Page page, int line_shift)
+{
+	page.horizontal_offs = MAX(0, page.horizontal_offs - line_shift);
+
+	return page;
+}
+
+Page page_right(Page page, int line_shift, int max_line_len)
+{
+	page.horizontal_offs = MIN(page.horizontal_offs + line_shift, max_line_len - 1);
+
+	return page;
+}
+
+void wput_text_line(WINDOW *win, Text text, int i, int horizontal_offs, int win_width)
+{
+	char line_start[10];
+	sprintf(line_start, " %3d: ", i+1);
+	wprintw(win, "%s", line_start);
+	int rest_width = win_width -1 - strlen(line_start);
+	if (horizontal_offs < strlen(text.lines[i]))
+		w_add_nchars(win, text.lines[i] + horizontal_offs, rest_width);
+	waddch(win, '\n');
+}
+
+
 void draw_text(Text text, const char *filename)
 {
-    WINDOW *win;
-    int c = 0;
+	WINDOW *win;
+	
+	int c = 0;
 
-    initscr();
+	initscr();
 
-    noecho();
-    cbreak();
-    printw("File: %s; line count: %d;", filename, text.line_count); //title
-    refresh();
+	noecho();
+	cbreak();
+	printw("File: %s; line count: %d;", filename, text.line_count); //title
+	refresh();
 
-    win = newwin(LINES-2*DX, COLS-2*DX, DX, DX);
-    keypad(win, TRUE);
-    scrollok(win, TRUE);
-    
-    //hide cursor
-    curs_set(0);
+	int WIN_WIDTH = COLS-2*DX;
+	int WIN_HEIGHT = LINES-2*DX;
+	win = newwin(WIN_HEIGHT, WIN_WIDTH, DX, DX);
+	dbg_win = win;
+	keypad(win, TRUE);
+	// scrollok(win, TRUE);
+	
+	//hide cursor
+	curs_set(0);
 
-    do {
-        box(win, 0, 0);
-    	wmove (win, 1, 1);
-     //    waddstr (win, "Hello, World!");
-        // waddch(win, 'K');
-        // w_add_nchars(win, "HELLO", 10);
-        w_add_nchars(win, text.lines[0], 10);
-        waddch(win, '\n');
-        // wprintw(win, "  Key: %d, Name: %s\n", c, keyname(c));
-        wrefresh(win);
-    } while((c = wgetch(win)) != 27);
+	Page page = create_page(0, WIN_HEIGHT-2, text.line_count, 0);
+	
+	do {
+		werase(win);
+		wmove (win, 1, 0);
 
-    endwin();
+		switch (c)
+		{
+			case KEY_UP:
+				page = page_up(page, 1);
+				break;
+			case KEY_DOWN:
+			case KEY_SPACE:
+				page = page_down(page, 1, text.line_count);
+				break;
+			case KEY_RIGHT:
+				page = page_right(page, 1, text.max_line_len);
+				break;
+			case KEY_LEFT:
+				page = page_left(page, 1);
+				break;
+			case KEY_NPAGE:
+				page = page_down(page, WIN_HEIGHT-2, text.line_count);
+				break;
+			case KEY_PPAGE:
+				page = page_up(page, WIN_HEIGHT-2);
+				break;
+			default:
+				break;
+		}
+
+		for (int i = page.first_line; i <= page.last_line; ++i)
+		{
+			wput_text_line(win, text, i, page.horizontal_offs, WIN_WIDTH);
+		}
+		// wprintw(win, "  Key: %d, Name: %s\n", c, keyname(c));
+
+		box(win, 0, 0);
+		wrefresh(win);
+		
+		move(LINES-DX+1,0);
+		printw(" Column: %3d", page.horizontal_offs + 1);
+		refresh();
+	} while((c = wgetch(win)) != KEY_ESC);
+
+	endwin();
 }
 
 int main(int argc, char **argv)
@@ -154,5 +279,5 @@ int main(int argc, char **argv)
 
 	draw_text(text, argv[1]);
 
-    free_text(text);
+	free_text(text);
 }
